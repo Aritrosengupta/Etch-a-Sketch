6bdf21111cbcec18a4d38c5a503ce0446f15fcff
Aritro Sengupta <aritro.as@gmail.com>
2024-11-04 20:08:43 +0530

    Created function for random color

--- script.js
+++ script.js
@@ -12,9 +12,33 @@ function CreateGrid(num){
         grid.style.height=`${size}px`;
         grid.style.width=`${size}px`;
         grid.style.border = "1px solid black";
-        grid.style.float='left';
+        
         grid.style.boxSizing = 'border-box';
+
+      const mouseHandler=()=>{
+        let color=randomColorGenerator();
+        grid.style.backgroundColor=color;
+        grid.removeEventListener('mouseover',mouseHandler);
+      }
+      grid.addEventListener('mouseover',mouseHandler);
         container.appendChild(grid);
-    }
+    }   
 }
+
+function randomColorGenerator(){
+    let options="0123456789ABCDEF";
+    color="#";
+    for(let i=0;i<6;i++){
+        color+=options[Math.floor(Math.random()*16)];
+        
+    }
+    return color;
+
+}
+
+// function mouseHandler(){
+//     let color=randomColorGenerator();
+//     grid.style.backgroundColor=color;
+//     grid.removeEventListener('mouseover',mouseHandler)
+// }
 CreateGrid(num);
